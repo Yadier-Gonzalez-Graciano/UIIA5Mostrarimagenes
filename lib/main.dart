c524ba0b680fd225c88636c0590e1061554415b5
YADIER_GONZALEZ_GRACIANO <a.21308051280488@cbtis128.edu.mx>
2024-04-15 05:48:19 +0000

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      title: "MaterialApp",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Mostrar imagenes"),
         ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: 300.0,
+              width: 350.0,
+              child: Image.asset(
+                'assets/lobo.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 350.0,
+              width: 350.0,
+              child: Image.network(
+                'https://i.pinimg.com/564x/01/02/d5/0102d59a1e73e55bafd2af4221671280.jpg',
+                fit: BoxFit.cover,
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
